7b7a4b900599752ed6fa93b58d1c956ab19b5b50
Dip Lukka <navinp@zignuts.com>
2024-02-14 15:44:37 +0530

    initial commit

---
+++ Headers/BaladorSDK.h
@@ -0,0 +1,18 @@
+//
+//  BaladorSDK.h
+//  BaladorSDK
+//
+//  Created by Zignuts Technolab on 30/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaladorSDK.
+FOUNDATION_EXPORT double BaladorSDKVersionNumber;
+
+//! Project version string for BaladorSDK.
+FOUNDATION_EXPORT const unsigned char BaladorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaladorSDK/PublicHeader.h>
+
+
